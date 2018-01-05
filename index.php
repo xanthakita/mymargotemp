e49832260643c9e126d245ff298b79451c166a6b
Jonathan Wagner <jonathan.wagner@windstream.com>
2018-01-05 16:44:18 -0500

    trying to get facebook posts to work

--- index.php
+++ index.php
@@ -55,36 +55,26 @@ $decoded = json_decode($result, true);
 $postId = explode("_", $decoded['data'][0]['id']); 
  
 //Tie it all together to construct the URL
-$url = "https://www.facebook.com/loris.lovely.lashes.muncie/posts/$postId[1]";
+$url2 = "https://www.facebook.com/loris.lovely.lashes.muncie/posts/$postId[1]";
  
 //Make the API call
-$result = file_get_contents($url);
+// $result = file_get_contents($url2);
  
 //Decode the JSON.
-$decoded = json_decode($result, true);
+// $decoded = json_decode($result, true);
  
 //Dump it out.
-echo $url."<br>";
+echo $url2."<br>";
 //var_dump($decoded);
 
 //echo "<iframe src='".$url."' width='100%' height='100%' seamless></iframe>";
-
-foreach($response->getDecodedBody()['data'] as $item) {
-    foreach ($item as $key => $value) {
-        if($key == 'full_picture') { echo '<img src="'.$value.'" width="300px" height="300px"/>'; }
-        elseif($key == 'created_time') { echo date('F jS, Y h:i:s', strtotime($value));} else {
-        echo $key .": ". $value;}
-        echo "<br/>";
-    }
-    echo "<br/>--------------<br/>";
- }   
-
 
 ?>
 <hr>
 
       <div id="fb-root"></div>
-<div class="fb-video" data-href='<?php echo $url; ?>' data-width="500" data-show-text="true"></div>
+      <h1>test</h1>
+<div class="fb-video" data-href='<?php echo $url2; ?>' data-width="500" data-show-text="true"></div>
 <hr>
 </body>
 </html>
