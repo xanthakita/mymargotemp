c1b1ea0f2c1257ae08bd7cd4abdb37703efd09df
Jonathan Wagner <jonathan.wagner@windstream.com>
2018-02-11 06:57:58 -0500

    added images.php for callback

--- images.php
+++ images.php
@@ -2,5 +2,10 @@
 	if ($_GET['hub_verify_token'] == '3lll')
 	{
 		echo $_GET['hub_challenge'];
+	} else {
+		$fp=fopen('fbtest.log',"a");
+		$output=var_dump($_POST);
+		fwrite($fp,$output);
+		flcose($fp);
 	}
 ?>
