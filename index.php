4ada47481ba94cfba4406d2b92961e5945263fde
Jonathan Wagner <jonathan.wagner@windstream.com>
2018-02-15 14:11:04 -0500

    switched albumid from mbile uploads to all photos.. testing

--- index.php
+++ index.php
@@ -7,11 +7,12 @@ $fb = new Facebook\Facebook([
   'default_graph_version' => 'v2.11',
   ]);
 
-// $accessToken = '138484903495684|mreViJjw_fCCbrBjOR9hKNSEb4k';
-$accessToken = 'EAAB984d6yAQBAMTI1nNOvNL12XSaz1RZBvCPZB1vC5Jpgsq3iS8yDIzDT0QA97EiqlEFutygabGga3gBE2r7MlpZB0gj4CGayBMoPC4JnTkO4ZC3FZCaiZA3idZCtWpGQyESYV3x4B5deifKI7jtH9Gv3iZBuQnu82GvQnNe3VnxICwNzSsZBBGKpQDZAYt4eJ7aoZD';
+$accessToken = '138484903495684|mreViJjw_fCCbrBjOR9hKNSEb4k';
+
 $fb->setDefaultAccessToken($accessToken);
 $id='230941890252535';
-$album_id = '811654502181268';
+//$album_id = '811654502181268';
+$album_id = '290876794259044';
 $photos = $fb->get("/$album_id/photos?fields=images&width", $accessToken)->getGraphEdge()->asArray();
 
 
