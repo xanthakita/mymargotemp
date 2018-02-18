8208335ec7a9808173835ecf90f7fb0733135769
Jonathan Wagner <jonathan.wagner@windstream.com>
2018-02-18 13:25:11 -0500

    testing looking for name

--- index_test.php
+++ index_test.php
@@ -12,16 +12,17 @@ $fb = new Facebook\Facebook([
 $accessToken = '138484903495684|mreViJjw_fCCbrBjOR9hKNSEb4k';
 $fb->setDefaultAccessToken($accessToken);
 $id='230941890252535';
-$album_id = '811654502181268';
+// $album_id = '811654502181268';
+$album_id = '1676498949030148';
 $photos = $fb->get("/$album_id/photos?fields=images&name&width", $accessToken)->getGraphEdge()->asArray();
 
 
     foreach($photos as $photo){
         echo "<br><img src='{$photo['images'][3]['source']}' width='25%' />".PHP_EOL;//Get largest by 0 index
         echo "<br>name:{$photo['name']}<br>".PHP_EOL;
-      // echo "<pre>";
-      // var_dump($photo);
-      // echo "</pre>";
+      echo "<pre>";
+      var_dump($photo);
+      echo "</pre>";
     }
 
     // for ($x=0; $x<9; $x++)
