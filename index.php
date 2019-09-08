d70170295be0d31667f2d8788071e24e3572b9d5
Jonathan Wagner <xanthakita@gmail.com>
2019-09-08 00:13:46 -0400

    turned on full error reporting

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // adding a comment in order to force git to update.
 
 require_once __DIR__ . '/vendor/autoload.php';
